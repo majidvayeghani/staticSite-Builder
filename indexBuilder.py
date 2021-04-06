26b6eedb664defe7183e3f71b61578f343b74818
majidpv <majidpv96@gmail.com>
2021-04-06 20:27:40 +0430

    change index.html location

--- indexBuilder.py
+++ indexBuilder.py
@@ -3,10 +3,10 @@ from jinja2 import Environment, FileSystemLoader
 
 def build():
     env = Environment(loader=FileSystemLoader('templates'))
-    template = env.get_template('index.html')
+    template = env.get_template('indexTemp.html')
     item = template.render()
 
     # save rendered block code 
-    fileName = "./index.html"
+    fileName = "./website-master/index.html"
     with open(fileName, "w") as newItem:
         newItem.write(item)
