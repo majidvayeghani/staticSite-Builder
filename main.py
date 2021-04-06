c021d0d83f4528f4b6fd7d02aa4a865dbf6e7b24
majidpv <majidpv96@gmail.com>
2021-04-07 00:52:47 +0430

    check valid img's address on web and download it to ./images. the new img's name is based on img's number that given by user.

--- main.py
+++ main.py
@@ -2,19 +2,39 @@ from containerBuilder import merg
 from costumeBuilder import addImg
 from indexBuilder import build
 import sys  
-
+import re
+import requests
+import os
 
 x = int(input(" 1 for add new, 2 for edit, 3 for delete: "))
+
 
 if(x == 1):
   #ToDo: copy img from other locations to project location.
   x = "y"
   while ( x == 'Y' or x == 'y'):
+    number = input ("input the image's number: ")
     name = input( "input the image's addrees: ") 
-
+    
+    #regex for valid image address
+    regex = "([^\\s]+(\\.(?i)(jpe?g|png|gif|bmp))$)"
+    reg = re.compile(regex)
+   
+    if(re.search(reg,name)):
+      downloaded_obj = requests.get(name)
+      f_format = os.path.splitext(name)[-1]
+      f_name = 'img_{}{}'.format(number,f_format)
+      fileName = 'img_{}'.format(number) #for costume's html file
+      imgName = "/home/majid/Desktop/project/images/" + f_name
+      print(imgName)
+      with open(imgName, "wb") as file:
+        file.write(downloaded_obj.content)
+    else:
+      break
+    
+    # for manage enter character in user input.
     total_input = []
     print( "input the image's caption': " )
-    # for manage enter character in user input.
     while True:
       txt = input()
       if txt:
@@ -24,7 +44,7 @@ if(x == 1):
 
     total_input = '<br>'.join(total_input) #<br> tag uses in html file.
 
-    addImg(name,total_input)
+    addImg(fileName,imgName,total_input)
 
     x = input( "Do you want to add new image? Y for continue, any Button for end. ")
 
