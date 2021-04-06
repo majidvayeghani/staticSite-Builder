d83bc05c150cfd2583e6734ab5d6912c4d4a9d84
majidpv <majidpv96@gmail.com>
2021-04-06 20:18:08 +0430

    handel ENTER character in user input

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from containerBuilder import merg
-from imgBuilder import addImg
+from costumeBuilder import addImg
 from indexBuilder import build
+import sys  
 
 
-x = int(input(" 1 for add new, 2 foe edi, 3 for delet: "))
+x = int(input(" 1 for add new, 2 for edit, 3 for delete: "))
 
 if(x == 1):
-  #ToDo: number must be uniqe.
-  #ToDo: copy img from other locations to project location
+  #ToDo: copy img from other locations to project location.
   x = "y"
   while ( x == 'Y' or x == 'y'):
-    name = input( "input the image number: ") 
-    txt = input("Input the image's caption: ")
+    name = input( "input the image's addrees: ") 
 
-    addImg(name,txt)
+    total_input = []
+    print( "input the image's caption': " )
+    # for manage enter character in user input.
+    while True:
+      txt = input()
+      if txt:
+        total_input.append(txt)
+      else:
+        break
+
+    total_input = '<br>'.join(total_input) #<br> tag uses in html file.
+
+    addImg(name,total_input)
 
     x = input( "Do you want to add new image? Y for continue, any Button for end. ")
 
